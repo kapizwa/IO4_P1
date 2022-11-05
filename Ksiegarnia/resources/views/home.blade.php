78e5ff6ab472d811beb418f75dcb37d6a968dafc
Pawel <studentpawel2000@gmail.com>
2022-11-05 12:24:10 +0100

    Dodanie bocznego menu

--- Ksiegarnia/resources/views/home.blade.php
+++ Ksiegarnia/resources/views/home.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Strona główna</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    Witaj {{ Auth::user()->name }}
                 </div>
             </div>
         </div>
---
+++ Ksiegarnia/resources/views/partials/menu.blade.php
@@ -0,0 +1,73 @@
+<div class="sidebar">
+    <nav class="sidebar-nav">
+
+        <ul class="nav">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    
+                Menu
+                </a>
+            </li>
+            
+                
+            
+            
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fa-fw fas fa-gear nav-icon">
+
+                        </i>
+                        Zarządzanie kontami
+                    </a>
+                </li>
+            
+            
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fa-fw fas fa-check nav-icon">
+
+                        </i>
+                        Złożone zamówienia
+                    </a>
+                </li>
+            
+        
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fa-fw fas fa-edit nav-icon">
+
+                        </i>
+                        Edycja zamówień
+                    </a>
+                </li>
+            
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="fa-fw fas fa-user nav-icon">
+
+                    </i>
+                    Użytkownicy
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="fa-fw fas fa-search nav-icon">
+
+                    </i>
+                    Wyszukiwanie książek
+                </a>
+            </li>
+            
+            <li class="nav-item">
+                <a href="#" class="nav-link" onclick="event.preventDefault(); document.getElementById('logoutform').submit();">
+                    <i class="nav-icon fas fa-fw fa-sign-out-alt">
+
+                    </i>
+                    Wyloguj
+                </a>
+            </li>
+        </ul>
+
+    </nav>
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+</div>
