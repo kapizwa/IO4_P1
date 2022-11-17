bd74c54c95084d71a1ec017e4e0a501fdf6f943e
DawidBarnas <dawidbarnas1998@gmail.com>
2022-11-17 01:56:41 +0100

    2.0.1 - Naprawiony blad wyswietlanej listy zamowien

--- Ksiegarnia/app/Http/Controllers/ZamowieniaController.php
+++ Ksiegarnia/app/Http/Controllers/ZamowieniaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use App\Models\Zamowienia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ZamowieniaController extends Controller
 {
@@ -14,7 +15,12 @@ class ZamowieniaController extends Controller
      */
     public function index_list()
     {
-        $orders=Zamowienia::All();
+        $id_current_user = Auth::id();
+
+        $orders = Zamowienia::where('user_id',$id_current_user)->get();
+        // return view('/users_list', compact('users'));
+
+        // $orders=Zamowienia::All();
         return view('/order_list', compact('orders'));
     }
 
